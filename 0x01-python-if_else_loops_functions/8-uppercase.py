b1faebfad92f37e91b23c9e5b39c09264ac7b4e2
afrejanas <afrejanas@gmail.com>
2023-06-06 17:55:45 -0700

    8. To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        char = ord(char)
+        if char >= 97 and char <= 122:
+            char = char - 32
+        char = chr(char)
+        print("{}".format(char), end='')
+    print("")
