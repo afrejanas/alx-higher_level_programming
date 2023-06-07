6197715d5a0fcd2f0a0ac203ef2af16cfae82cb9
afrejanas <afrejanas@gmail.com>
2023-06-06 17:12:49 -0700

    fixed

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i % 10 == i:
-        print("0{:d}".format(i), end=', ')
-    elif i == 99:
-        print("{:d}\n".format(i))
+    if i == 99:
+        print("99")
     else:
-        print("{:d}".format(i), end=', ')
+        print("{0:02d}, ".format(i), end='')
