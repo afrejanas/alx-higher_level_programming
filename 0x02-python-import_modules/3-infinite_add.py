b7bc0705cc74aaa20b8b9cfbe84a67ed2ee3fa69
afrejanas <afrejanas@gmail.com>
2023-06-08 16:01:23 -0700

    fixed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     from sys import argv
     argc = len(argv)
     if argc == 1:
-        print("0")
-    i = 1
-    sum = 0
-    if argc > 1:
+        print("{:d}".format(argc - 1))
+    else:
+        i = 1
+        sum = 0
         while i < argc:
             sum += int(argv[i])
             i = i + 1
-        print('{}'.format(sum))
+        print('{:d}'.format(sum))
