09fe397430b6fcfbdb401253c5915ff0a04c2995
afrejanas <afrejanas@gmail.com>
2023-06-08 16:37:42 -0700

    4. Who are you?

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
+    length = len(names)
+    for i in range(0, length):
+        if names[i][:2] != "__":
+            print(names[i])
